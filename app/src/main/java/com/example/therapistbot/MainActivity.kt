50ed48885d837ea0bada2d83ea113bb83cf49d82
Micah Nyabuto <atembamicah@gmail.com>
2025-02-26 23:56:43 +0300

    update

--- app/src/main/java/com/example/therapistbot/MainActivity.kt
+++ app/src/main/java/com/example/therapistbot/MainActivity.kt
@@ -11,6 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.therapistbot.ui.Screens.SignInScreen
+import com.example.therapistbot.ui.Screens.SignUpScreen
+import com.example.therapistbot.ui.navigation.TherapistApp
 import com.example.therapistbot.ui.theme.TherapistBotTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +22,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TherapistBotTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                TherapistApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TherapistBotTheme {
-        Greeting("Android")
-    }
-}
