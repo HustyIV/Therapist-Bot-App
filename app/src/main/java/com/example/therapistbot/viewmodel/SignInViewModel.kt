c26c046812aedb5dbd74f34d3c0b4032f2285398
Micah Nyabuto <atembamicah@gmail.com>
2025-02-26 23:26:03 +0300

    Added viewmodels

---
+++ app/src/main/java/com/example/therapistbot/viewmodel/SignInViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.therapistbot.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInViewModel: ViewModel() {
+    private val auth = FirebaseAuth.getInstance()
+
+
+    //Hold user password and email
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val signInResult = MutableLiveData<Result<Boolean>>()
+
+
+
+    //Update the LiveData
+    fun updateEmail(newEmail : String){
+        email.value = newEmail
+    }
+
+    fun updatePassword(newPassword : String){
+        password.value = newPassword
+    }
+
+    fun login(email:String, password:String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    signInResult.value = Result.success(true)
+                } else {
+                    signInResult.value = Result.failure(task.exception?: Exception("Login failed"))
+                }
+
+            }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/therapistbot/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.therapistbot.viewmodel
+
+import android.database.CursorJoiner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpViewModel : ViewModel(){
+
+    private val auth = FirebaseAuth.getInstance()
+
+    //Hold user password and email
+    val email = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+    val confirmPassword = MutableLiveData<String>()
+    val signUpResult = MutableLiveData<Result<Boolean>>()
+
+
+    //Update the LiveData
+    fun updateEmail(newEmail : String){
+        email.value = newEmail
+    }
+
+    fun updatePassword(newPassword : String){
+        password.value = newPassword
+    }
+    fun updateConfirmPassword(newConfirmPassword : String){
+        confirmPassword.value = newConfirmPassword
+
+    }
+    fun signUpUser(email: String, password: String){
+        if (password != confirmPassword.value){
+            signUpResult.value = Result.failure(Exception("Passwords do not match"))
+            return
+        }
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    signUpResult.value = Result.success(true)
+                } else {
+                    signUpResult.value = Result.failure(
+                        task.exception ?: Exception("Sign up failed")
+                    )
+
+                }
+            }
+    }
+
+
+}
