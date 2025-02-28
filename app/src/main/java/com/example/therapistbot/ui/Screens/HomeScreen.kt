0fe4dcf8a12785c0ea6aca807e240aa6d8e6de68
Micah Nyabuto <atembamicah@gmail.com>
2025-02-28 23:24:42 +0300

    Added greeting message

--- app/src/main/java/com/example/therapistbot/ui/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/therapistbot/ui/Screens/HomeScreen.kt
@@ -8,7 +8,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import java.util.Calendar
 
 @Composable
 fun HomeScreen(
@@ -22,6 +25,28 @@ fun HomeScreen(
         contentAlignment = Alignment.Center
 
     ) {
-        Text(text = "Home Screen ")
+        Greeting()
+    }
+}
+
+@Composable
+fun Greeting() {
+    val greetingText = greetingMessage()
+
+    Text(
+        text = greetingText,
+        fontSize = 22.sp,
+        fontWeight = FontWeight.SemiBold,
+    )
+
+}
+
+fun greetingMessage():String{
+    val hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+    return when (hour){
+        in 0..5 -> "Good Night!"
+        in 6..11 -> "Good Morning!"
+        in 12..17 -> "Good Afternoon!"
+        else -> "Good Evening!"
     }
 }
