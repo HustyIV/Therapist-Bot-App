88f66db45516429431dd26dc3e0e582162c55eba
Micah Nyabuto <atembamicah@gmail.com>
2025-03-27 23:34:30 +0300

    Fixed Chatscreen

--- app/src/main/java/com/example/therapistbot/ui/Screens/ChatScreen.kt
+++ app/src/main/java/com/example/therapistbot/ui/Screens/ChatScreen.kt
@@ -1,38 +1,79 @@
 package com.example.therapistbot.ui.Screens
 
+import android.R.attr.text
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.therapistbot.ui.theme.LightBlue
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun ChatScreen() {
-    //var text by remember { mutableStateOf("") }
+fun ChatScreen(
+    modifier: Modifier = Modifier
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
 
     Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = LightBlue,
+                    titleContentColor = Color.White,
+                ),
+                title = {
+                    Text(text = "Chat with Thera")
+                },
+                navigationIcon = {
+                    IconButton(onClick = {  }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "navigate back"
+                        )
+                    }
+                },
+            )
+        },
+    //var text by remember { mutableStateOf("") }
         bottomBar = {
             BottomAppBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                containerColor = LightBlue,
+                contentColor = MaterialTheme.colorScheme.onSurface,
             ) {
                 Row(
                     modifier = Modifier
@@ -43,13 +84,21 @@ fun ChatScreen() {
                 ) {
                     OutlinedTextField(
                         value = "",
-                        onValueChange = { /* Handle text change */ },
+                        onValueChange = {  },
+                        placeholder = { Text(text = "Start chat here...") },
+                        keyboardOptions = KeyboardOptions.Default.copy(
+                            keyboardType = KeyboardType.Text
+                        ),
                         modifier = Modifier
                             .weight(1f)
+                            .padding(end = 8.dp),
+                        shape = RoundedCornerShape(70.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            containerColor = Color.White,
+                            focusedBorderColor = Color.Transparent,
+                            unfocusedBorderColor = Color.Transparent
+                        ),
 
-                            .padding(end = 8.dp,
-                                bottom = 16.dp,),
-                        shape = RoundedCornerShape(65.dp)
                     )
 
                     IconButton(
@@ -59,7 +108,7 @@ fun ChatScreen() {
                         modifier = Modifier
                             .size(48.dp)
                             .background(
-                                color = MaterialTheme.colorScheme.primary,
+                                color = LightBlue,
                                 shape = CircleShape
                             )
                     ) {
